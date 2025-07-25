dbc7437980b1f260e005d38e94888e311ce0303f
keerthana-vr <kvr9143@gmail.com>
2025-07-25 18:01:51 +0530

    Added Login Page UI

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,16 @@
+function LoginPage(){
+    return (
+        <div>
+            <h2>Login</h2>
+            <input type="text"
+placeholder="Username" />
+             <br  />
+             <input type="password"
+placeholder="Password"  />
+              <br  />
+              <button>Login</button>
+        </div>
+    );
+}
+
+export default LoginPage;
